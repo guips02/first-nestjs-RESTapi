8f96fb19ca5a76b80a690d5eacb4925efe64513a
Guilherme P Silva <guilhermeps002@outlook.com>
2024-09-13 21:52:47 -0300

    Auth resources initialized

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signup() {
+    return { msg: 'I am sign up' };
+  }
+  signin() {
+    return { msg: 'I am sign in' };
+  }
+}
